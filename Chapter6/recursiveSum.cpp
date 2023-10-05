f060213df7ddfd26a96580cd2bc0fe5c47c7b683
Imsyp <tkddud386@gmail.com>
2023-10-05 21:00:08 +0900

    20231005

---
+++ Chapter6/recursiveSum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int sum(int n)
+{
+    int ret = 0;
+    for(int i=0; i<= n; i++)
+    {
+        ret += i;
+    }
+    return ret;
+}
+
+int recursiveSum(int n)
+{
+    if(n == 1) return 1;
+    return n + recursiveSum(n-1);
+}
