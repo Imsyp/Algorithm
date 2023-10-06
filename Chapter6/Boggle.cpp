3ec8f1aebf13480d95873b7368a54590d046f5a0
Imsyp <tkddud386@gmail.com>
2023-10-06 21:56:54 +0900

    20231006

--- Chapter6/Boggle.cpp
+++ Chapter6/Boggle.cpp
@@ -4,20 +4,52 @@
 
 using namespace std;
 
-int hasWord(int y, int x, vector<string>& word)
+class Point
 {
-    int TRUE = 1;
-    int FALSE = 0;
+    public:
+        int x;
+        int y;
+        char data;
 
-    
-    if(getSpell(y, x) != word[location])
-        return FALSE;   
-    else if(word.size() == 0)
-        return TRUE;
+        void initPoint(int x, int y, char data)
+        {
+            x = this -> x;
+            y = this -> y;
+            data = this -> data;
+        }
+
+        char getSpell(int y, int x)
+        {
+            this->x
+        }
+};
+
+bool inRange(int y, int x)
+{
+    if(!(y<6) || !(x<6))
+        return false;
+    else
+        return true;
+}
+
+
+
+bool hasWord(int y, int x, const string& word)
+{
+    if(!inRange(y, x))
+        return false;
+
+    if(getSpell(y, x) != word[0])
+        return false;   
+    else if(word.size() == 1)
+        return true;
     else
     {
         for(int i=-1; i<=1; i++)
             for(int j=-1; j<=1; j++)
-                hasWord(y+i, x+j, word)
+                if(hasWord(y+i, x+j, word.substr(1)))
+                    return true;
     }
+
+    return false;
 }
