005cf320c6697822fe251554f0bf1cdb9e525523
Imsyp <tkddud386@gmail.com>
2023-10-15 16:41:05 +0900

    20231015

--- Chapter6/BoardCover.cpp
+++ Chapter6/BoardCover.cpp
@@ -4,5 +4,23 @@
 
 using namespace std;
 
-int H, W;
-int board[H][W];
+bool board[20][20];
+
+//남은 빈 칸 개수 반환
+int NumofBlank(int H, int W, bool BlankBoard[20][20])
+{
+    int blank = 0;
+
+    for(int i=0; i<H; i++)
+        for(int j=0; j<W; j++)
+        {
+            if(BlankBoard[i][j] == true) blank++;
+        }
+    return blank;
+}
+
+//채울 수 있는 경우의 수 반환
+int NumofCases(int H, int W, bool BlankBoard[20][20])
+{
+
+}
