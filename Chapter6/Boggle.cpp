b99f923370c101a21afcb3bed04f43772ef3e211
Imsyp <tkddud386@gmail.com>
2023-10-16 16:06:51 +0900

    20231016

--- Chapter6/Boggle.cpp
+++ Chapter6/Boggle.cpp
@@ -4,42 +4,32 @@
 
 using namespace std;
 
-class Point
-{
-    public:
-        int x;
-        int y;
-        char data;
+string board[5];
 
-        void initPoint(int x, int y, char data)
-        {
-            x = this -> x;
-            y = this -> y;
-            data = this -> data;
-        }
+/*
+const int dx = { -1, -1, -1, 1, 1, 1, 0, 0 };
+const int dy = { -1, 0, 1, -1, 0, 1, -1, 1 };
+*/
 
-        char getSpell(int y, int x)
-        {
-            this->x
-        }
-};
-
+// y,x가 범위 안에 있으면 true, 밖이면 false
 bool inRange(int y, int x)
 {
-    if(!(y<6) || !(x<6))
-        return false;
-    else
+    if(y > 0 && y < 5 && x > 0 && x < 5)
         return true;
+    else
+        return false;
 }
 
-
-
+//y,x에서 시작해서 word가 존재하면 true
 bool hasWord(int y, int x, const string& word)
 {
+    //y,x가 범위 밖이면 false 반환
+    //y,x 위치의 글자가 단어의 첫 글자가 아닌 경우 false 반환
+    //word의 길이가 1글자면 true 반환
+    //검증한 다음 글자에 대해서 y,x의 주변 위치에서 hasWord 실행
     if(!inRange(y, x))
         return false;
-
-    if(getSpell(y, x) != word[0])
+    else if(board[y][x] != word[0])
         return false;   
     else if(word.size() == 1)
         return true;
@@ -47,9 +37,68 @@ bool hasWord(int y, int x, const string& word)
     {
         for(int i=-1; i<=1; i++)
             for(int j=-1; j<=1; j++)
+            {
+                if(i == 0 && j == 0)
+                {
+                    continue;
+                }
                 if(hasWord(y+i, x+j, word.substr(1)))
+                {
                     return true;
+                }
+            }
     }
-
     return false;
+}
+
+int main(void)
+{
+    int C;
+    cin >> C;
+
+    while(C--)
+    {
+        //board 채우기
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                cin>>board[i][j];
+            }
+        }
+
+        int N;
+        cin >> N;
+        //N개의 word 검증
+        for(int n = 0; n < N; n++)
+        {
+            bool Check = false;
+            string word;
+            cin >> word;
+            for(int a = 0; a < 5; a++)
+            {
+                for(int b = 0; b < 5; b++)
+                {
+                    Check = hasWord(a, b, word);
+                    if(Check)
+                    {
+                        break;
+                    }
+                }
+                if(Check)
+                {
+                    break;
+                }
+            }
+            if(Check)
+            {
+                cout << word << " YES" << endl;
+            }
+            else
+            {
+                cout<< word << " NO" << endl;
+            }
+        }
+    }
+    return 0;
 }
