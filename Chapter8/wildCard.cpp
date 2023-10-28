944c7d5bf5a330756d9b06ac710cc7f65b134f4f
Imsyp <tkddud386@gmail.com>
2023-10-28 21:57:01 +0900

    20231028

--- Chapter8/wildCard.cpp
+++ Chapter8/wildCard.cpp
@@ -2,13 +2,12 @@
 #include <cstring>
 #include <vector>
 #include <cassert>
+#include <algorithm>
 
 using namespace std;
 
 //파일명이 패턴에 대응되면 1, 아니면 0을 반환
-int match(const string& wildCard, const string& file) {
-    //base case: 
-
+int match(const string& wildCard, const string& file) { 
     //file[pos]와 wildCar[pos]가 대응되지 않을 때 종료 (거짓)
     //wildCard의 끝에 도달하면 종료 -> 패턴에 *가 없는 경우 (file과 문자열 길이가 같아야 참)
     //file의 끝에 도달하면 종료 -> 패턴은 남았지만 문자열이 끝난 경우 (남은 패턴이 모두 *인 경우를 제외하고 답은 거짓)
@@ -17,16 +16,32 @@ int match(const string& wildCard, const string& file) {
     while(pos < file.size() && pos < wildCard.size() && (wildCard[pos] == '?' || wildCard[pos] == file[pos])) {
         pos++;
     }
+
+    //wildCard 패턴 끝에 도달해서 끝난 경우 -> 문자열도 끝났어야 대응
+    if(pos == wildCard.size()) {
+        return pos == file.size();
+    }
+
+    //*를 만나서 끝난 경우 -> *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인
+    if(wildCard[pos] == '*') {
+        for(int skip = 0; pos+skip <= file.size(); skip++) {
+            if(match(wildCard.substr(pos+1), file.substr(pos+skip))) {
+                return true;
+            }
+        }
+    }
+
+    //이 외의 경우는 모두 거짓
+    return false;
 }
 
-bool main(void)
-{
+int main(void) {
     int cases;
     cin >> cases;
     assert(cases >= 1 && cases <= 10);
 
     while(cases--) {
-        string fileZip[50];
+        vector<string> fileZip;
 
         string W;
         cin >> W;
@@ -41,8 +56,16 @@ bool main(void)
             cin >> file;
             assert(file.size() >= 1 && file.size() <= 100);
             if(match(W, file)) {
-                fileZip[i] = file;
+                fileZip.push_back(file);
             }
         }
+
+        sort(fileZip.begin(), fileZip.end());   //아스키 코드 순으로 정렬!
+
+        for(int i = 0; i < fileZip.size(); i++) {
+            cout << fileZip[i] << endl;
+        }
     }
+
+    return 0;
 }
