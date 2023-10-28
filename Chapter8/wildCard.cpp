b548cda1f34912910f2d65854e5e1e9fdab11a2f
Imsyp <tkddud386@gmail.com>
2023-10-28 20:15:27 +0900

    20231028

--- Chapter8/wildCard.cpp
+++ Chapter8/wildCard.cpp
@@ -8,9 +8,18 @@ using namespace std;
 //파일명이 패턴에 대응되면 1, 아니면 0을 반환
 int match(const string& wildCard, const string& file) {
     //base case: 
+
+    //file[pos]와 wildCar[pos]가 대응되지 않을 때 종료 (거짓)
+    //wildCard의 끝에 도달하면 종료 -> 패턴에 *가 없는 경우 (file과 문자열 길이가 같아야 참)
+    //file의 끝에 도달하면 종료 -> 패턴은 남았지만 문자열이 끝난 경우 (남은 패턴이 모두 *인 경우를 제외하고 답은 거짓)
+    //wildCard[pos]가 *인 경우 종료 
+    int pos = 0;
+    while(pos < file.size() && pos < wildCard.size() && (wildCard[pos] == '?' || wildCard[pos] == file[pos])) {
+        pos++;
+    }
 }
 
-int main(void)
+bool main(void)
 {
     int cases;
     cin >> cases;
