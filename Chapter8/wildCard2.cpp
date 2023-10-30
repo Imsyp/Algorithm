9208d77d22fe55230c181a4cce8355738350170d
Imsyp <tkddud386@gmail.com>
2023-10-30 19:15:32 +0900

    20231030

--- Chapter8/wildCard2.cpp
+++ Chapter8/wildCard2.cpp
@@ -6,68 +6,96 @@
 
 using namespace std;
 
-int cache[101][101];
+typedef long long ll;
 
-//파일명이 패턴에 대응되면 1, 아니면 0을 반환
-int match(const string& wildCard, const string& file) { 
-    //file[pos]와 wildCar[pos]가 대응되지 않을 때 종료 (거짓)
-    //wildCard의 끝에 도달하면 종료 -> 패턴에 *가 없는 경우 (file과 문자열 길이가 같아야 참)
-    //file의 끝에 도달하면 종료 -> 패턴은 남았지만 문자열이 끝난 경우 (남은 패턴이 모두 *인 경우를 제외하고 답은 거짓)
-    //wildCard[pos]가 *인 경우 종료 
-    int pos = 0;
-    while(pos < file.size() && pos < wildCard.size() && (wildCard[pos] == '?' || wildCard[pos] == file[pos])) {
-        pos++;
-    }
+string W, S;
 
-    //wildCard 패턴 끝에 도달해서 끝난 경우 -> 문자열도 끝났어야 대응
-    if(pos == wildCard.size()) {
-        return pos == file.size();
-    }
-
-    //*를 만나서 끝난 경우 -> *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인
-    if(wildCard[pos] == '*') {
-        for(int skip = 0; pos+skip <= file.size(); skip++) {
-            if(match(wildCard.substr(pos+1), file.substr(pos+skip))) {
-                return true;
-            }
-        }
-    }
-
-    //이 외의 경우는 모두 거짓
-    return false;
+// 와일드카드 패턴 w 가 문자열 s 에 대응되는지 여부를 반환한다
+bool match(const string& w, const string& s) {
+	// w[pos] 와 s[pos] 를 맞춰나간다
+	int pos = 0;
+	while(pos < s.size() && pos < w.size() && (w[pos] == '?' || w[pos] == s[pos]))
+		++pos;
+	// 더 이상 대응할 수 없으면 왜 while 문이 끝났는지 확인한다
+	// 1. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 참
+	if(pos == w.size())
+		return pos == s.size();
+	// 2. * 를 만나서 끝난 경우: * 에 몇 글자를 대응해야 할지 재귀호출하면서 확인한다
+	if(w[pos] == '*') {
+		for(int skip = 0; pos+skip <= s.size(); ++skip)
+			if(match(w.substr(pos+1), s.substr(pos+skip)))
+				return true;
+		return false;
+	}
+	// 3. 이외의 경우에는 모두 대응되지 않는다
+	return false;
 }
 
-int main(void) {
-    int cases;
-    cin >> cases;
-    assert(cases >= 1 && cases <= 10);
+int cache[5001][5001];
 
-    while(cases--) {
-        vector<string> fileZip;
+// 와일드카드 패턴 W[w..] 가 문자열 S[s..] 에 대응되는지 여부를 반환한다
+bool matchMemoized(int w, int s) {
+	int& ret = cache[w][s];
+	if(ret != -1) return ret;
+	// W[w] 와 S[s] 를 맞춰나간다
+	while(s < S.size() && w < W.size() && (W[w] == '?' || W[w] == S[s])) {
+		++w;
+		++s;
+	}
+	// 더 이상 대응할 수 없으면 왜 while 문이 끝났는지 확인한다
+	// 1. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 참
+	if(w == W.size()) return ret = (s == S.size());
+	// 2. * 를 만나서 끝난 경우: * 에 몇 글자를 대응해야 할지 재귀호출하면서 확인한다
+	if(W[w] == '*') {
+		for(int skip = 0; skip+s <= S.size(); ++skip)
+			if(matchMemoized(w+1, s+skip))
+				return ret = 1;
+		return ret = 0;
+	}
+	// 3. 이외의 경우에는 모두 대응되지 않는다
+	return ret = 0;
+}
+// 와일드카드 패턴 W[w..] 가 문자열 S[s..] 에 대응되는지 여부를 반환한다
+bool matchMemoized2(int w, int s) {
+	int& ret = cache[w][s];
+	if(ret != -1) return ret;
+	if(w < W.size() && s < S.size() && (W[w] == '?' || W[w] == S[s]))
+		return ret = matchMemoized2(w+1, s+1);
+	// 더 이상 대응할 수 없으면 왜 while 문이 끝났는지 확인한다
+	// 1. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 참
+	if(w == W.size()) return ret = (s == S.size());
+	// 2. * 를 만나서 끝난 경우: * 에 몇 글자를 대응해야 할지 재귀호출하면서 확인한다
+	if(W[w] == '*') {
+		if(matchMemoized2(w+1, s) ||
+			(s < S.size() && matchMemoized2(w, s+1)))
+			return ret = 1;
+	}
+	// 3. 이외의 경우에는 모두 대응되지 않는다
+	return ret = 0;
+}
 
-        string W;
-        cin >> W;
-        assert(W.size() >= 1 && W.size() <=100);
-
-        int N;
-        cin >> N;
-        assert(N >= 1 && N <= 50);
-
-        for(int i = 0; i < N; i++) {
-            string file;
-            cin >> file;
-            assert(file.size() >= 1 && file.size() <= 100);
-            if(match(W, file)) {
-                fileZip.push_back(file);
-            }
-        }
-
-        sort(fileZip.begin(), fileZip.end());   //아스키 코드 순으로 정렬!
-
-        for(int i = 0; i < fileZip.size(); i++) {
-            cout << fileZip[i] << endl;
-        }
-    }
-
-    return 0;
+int main(int argc, char* argv[])
+{
+	int cases;
+	cin >> cases;
+	REP(cc,cases)
+	{
+		cin >> W;
+		vector<string> ret;
+		int cc2;
+		cin >> cc2;
+		while(cc2--)
+		{
+			cin >> S;
+			CLEAR(cache,-1);
+			if(argc == 1) {
+				if(matchMemoized(0, 0)) ret.push_back(S);
+			}
+            else {
+				if(matchMemoized2(0, 0)) ret.push_back(S);
+			}
+		}
+		sort(all(ret));
+		REP(i,ret.sz) cout << ret[i] << endl;
+	}
 }
